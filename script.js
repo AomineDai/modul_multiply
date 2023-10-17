3f9ff88a018c87070500f853330a2bbb4089c10d
AomineDai <132513241+AomineDai@users.noreply.github.com>
2023-10-17 20:54:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var multiply = new Multiply({ 
+    elem: document.querySelector('#wrap-multiply'),
+    count: 3,
+    title: 'Считаем до трех',
+    captionAlign: 'left',
+    captionColor: '#38C864',
+  });
+
+
+var multiply2 = new Multiply({ 
+    elem: document.querySelector('#table-multiply'),
+    captionSize: '2em',
+  });
+
+
+
+var multiply3 = new Multiply({ 
+    elem: document.querySelector('.table'),
+    count:5,
+    title:' O ! До пяти',
+    captionAlign: 'right',
+    captionColor: 'red',
+  });
